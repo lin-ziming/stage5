36b2457b33736778c05f4cd9862621d7917e387c
linziming <871440130@qq.com>
2020-12-02 22:19:16 +0800

    sp09 feign

--- springcloud1/sp09-feign/src/main/java/cn/tedu/sp09/feign/ItemClientFB.java
+++ springcloud1/sp09-feign/src/main/java/cn/tedu/sp09/feign/ItemClientFB.java
@@ -5,6 +5,7 @@ import cn.tedu.web.util.JsonResult;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 public class ItemClientFB implements ItemClient{
     @Override
