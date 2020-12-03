c2cdf57100a13ec08f1709d6a6af1184e491d763
linziming <871440130@qq.com>
2020-12-03 23:11:36 +0800

    rabbitmq, 工作模式，生产者消费者

---
+++ rabbitmq/rabbitmq-api/src/main/java/m2/Consumer.java
@@ -0,0 +1,56 @@
+package m2;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 连接
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140"); // wht6.cn
+        // f.setPort(5672); //默认端口可以省略
+        f.setUsername("admin");
+        f.setPassword("admin");
+        Channel c = f.newConnection().createChannel();
+
+        // 定义队列
+        c.queueDeclare("helloworld", false, false, false, null);
+
+        DeliverCallback deliverCallback = new DeliverCallback() {
+            @Override
+            public void handle(String consumerTag, Delivery message) throws IOException {
+                // 遍历消息字符串，每个点字符都暂停一秒
+                String msg = new String(message.getBody());
+                System.out.println("收到： "+msg);
+                for (int i = 0; i <msg.length(); i++) {
+                    if ('.' == msg.charAt(i)) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+
+                //向服务器发送消息的回执
+                // c.basicAck(回执(long类型的编码), 是否一次确认多条消息);
+                c.basicAck(message.getEnvelope().getDeliveryTag(), false);
+                System.out.println("消息处理结束");
+            }
+        };
+        CancelCallback cancelCallback = new CancelCallback() {
+            @Override
+            public void handle(String consumerTag) throws IOException {
+            }
+        };
+
+        // 每次只接收处理1条消息，处理完成之前不接收下一条
+        // 必须在手动 ACK 模式下才有效
+        c.basicQos(1);
+
+        // 消费数据
+        // 第二个参数： true - 自动确认     false - 手动确认
+        c.basicConsume("helloworld", true, deliverCallback, cancelCallback);
+    }
+}
---
+++ rabbitmq/rabbitmq-api/src/main/java/m2/Producer.java
@@ -0,0 +1,37 @@
+package m2;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 连接
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140"); // wht6.cn
+        // f.setPort(5672); //默认端口可以省略
+        f.setUsername("admin");
+        f.setPassword("admin");
+        Channel c = f.newConnection().createChannel();
+        // 定义队列
+        // 队列如果在服务器端已经存在，属性不可变
+        c.queueDeclare("helloworld", false, false, false, null);
+
+        // 发送消息
+        // 循环输入消息发送
+        /*
+        输入消息：ger.err.e....er........er...e........
+
+        消费者受到消息后，遍历字符串，每个点字符，都暂停一秒，来模拟耗时消息
+         */
+        while (true) {
+            System.out.print("输入消息：");
+            String msg = new Scanner(System.in).nextLine();
+            c.basicPublish("", "helloworld", null, msg.getBytes());
+        }
+    }
+}
