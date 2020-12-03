c53a7dc94055e00e5649316210f6bccb18fe3ac5
linziming <871440130@qq.com>
2020-12-03 10:46:45 +0800

    替换老师的rabbitmq-api代码

--- rabbitmq/rabbitmq-api/src/main/java/m1/Consumer.java
+++ rabbitmq/rabbitmq-api/src/main/java/m1/Consumer.java
@@ -5,39 +5,34 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-/**
- * @author Haitao
- * @date 2020/11/26 16:52
- */
 public class Consumer {
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) throws Exception {
         // 连接
         ConnectionFactory f = new ConnectionFactory();
-        f.setHost("192.168.64.140");
+        f.setHost("192.168.64.140");   //   wht6.cn
         f.setUsername("admin");
         f.setPassword("admin");
         Channel c = f.newConnection().createChannel();
 
-        // 创建helloworld队列（队列如果已经存在，则不会重复创建）
-        c.queueDeclare("helloworld",false,false,false,null);
+        // 创建 helloworld 队列（队列如果已经存在，不会重复创建）
+        c.queueDeclare("helloworld", false, false, false, null);
 
-        DeliverCallback callback = new DeliverCallback() {
+        DeliverCallback deliverCallback = new DeliverCallback() {
             @Override
-            public void handle(String s, Delivery delivery) throws IOException {
-                byte[] a = delivery.getBody();
-                String msg = new String(a,"UTF-8");
-                System.out.println("收到"+msg);
+            public void handle(String consumerTag, Delivery message) throws IOException {
+                byte[] a = message.getBody();
+                String msg = new String(a);
+                System.out.println("收到： "+msg);
+            }
+        };
+        CancelCallback cancelCallback = new CancelCallback() {
+            @Override
+            public void handle(String consumerTag) throws IOException {
             }
         };
 
-        CancelCallback cancel = new CancelCallback() {
-            @Override
-            public void handle(String s) throws IOException {
-
-            }
-        };
-
-        //消费数据，等待从队列接收数据
-        c.basicConsume("helloworld",true,callback,cancel);
+        // 消费数据，等待从队列接收数据
+        // 第二个参数true： 由服务器对消息进行自动确认，确认后会直接删除消息
+        c.basicConsume("helloworld", true, deliverCallback, cancelCallback);
     }
 }
--- rabbitmq/rabbitmq-api/src/main/java/m1/Producer.java
+++ rabbitmq/rabbitmq-api/src/main/java/m1/Producer.java
@@ -5,45 +5,38 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
-/**
- * @author Haitao
- * @date 2020/11/26 16:12
- */
 public class Producer {
-    public static void main(String[] args) throws IOException, TimeoutException {
-        //建立连接
+    public static void main(String[] args) throws Exception {
+        // 建立连接
         ConnectionFactory f = new ConnectionFactory();
-        f.setHost("192.168.64.140");
+        f.setHost("192.168.64.140");  //   wht6.cn
         f.setPort(5672);
         f.setUsername("admin");
         f.setPassword("admin");
         Connection conn = f.newConnection();
-        Channel ch = conn.createChannel();
+        Channel c = conn.createChannel();
 
-        //让服务器创建队列 helloworld队列（已经存在则不重复创建）
-        //队列参数：helloworld, 是否是持久队列，是否排他(独占)队列，是否自动删除
-
-        ch.queueDeclare(
+        // 让服务器创建队列 helloworld（已经存在则不重复创建）
+        // 队列的参数: helloworld, 是否是持久队列, 是否排他（独占）队列, 是否自动删除
+        c.queueDeclare(
                 "helloworld",
                 false,
                 false,
                 false,
                 null); //其他参数属性，没有给null
 
-        /**
-         * 消息发送到 helloworld 队列
-         * 参数：
-         *  1. 默认的交换机(讲第三个模式时再解释)
-         *  3. 其他的消息属性配置,如果没有给null值
-         */
-        ch.basicPublish(
+        // 消息发送到 helloworld 队列
+        // 参数：
+        //   1. 默认的交换机(讲第三个模式时再解释)
+        //   3. 其他的消息属性配置,如果没有给null值
+        c.basicPublish(
                 "",
-                "helloword",
+                "helloworld",
                 null,
                 "Hello world!".getBytes());
 
         System.out.println("消息已发送");
+
     }
 }
