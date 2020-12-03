c098858370a120e008568d2f7d68a11fc128aa7a
linziming <871440130@qq.com>
2020-12-03 17:37:25 +0800

    sp11 zuul集成hystrix,添加商品的降级类

--- springcloud1/sp11-zuul/src/main/java/en/tedu/sp11/fallback/ItemFB.java
+++ springcloud1/sp11-zuul/src/main/java/en/tedu/sp11/fallback/ItemFB.java
@@ -1,67 +1,76 @@
-//package en.tedu.sp11.fallback;
-//
-//import cn.tedu.web.util.JsonResult;
-//import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.client.ClientHttpResponse;
-//
-//import java.io.ByteArrayInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//
-///**
-// * @author Haitao
-// * @date 2020/11/27 11:39
-// */
-//public class ItemFB implements FallbackProvider {
-//    /**
-//     * 返回 service-id
-//     * 针对哪个服务进行降级
-//     *
-//     * "*"或 null是针对说有服务进行降级
-//     * @return
-//     */
-//    @Override
-//    public String getRoute() {
-//        return "item-service";
-//    }
-//
-//    @Override
-//    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
-//        return new ClientHttpResponse() {
-//            @Override
-//            public HttpStatus getStatusCode() throws IOException {
-//                return HttpStatus.OK;
-//            }
-//            @Override
-//            public int getRawStatusCode() throws IOException {
-//                return HttpStatus.OK.value();
-//            }
-//            @Override
-//            public String getStatusText() throws IOException {
-//                return HttpStatus.OK.getReasonPhrase();
-//            }
-//
-//            @Override
-//            public void close() {
-//
-//            }
-//
-//            @Override
-//            public InputStream getBody() throws IOException {
-//                String json = JsonResult.ok().msg("调用后台商品服务失败").toString();
-//                return new ByteArrayInputStream(json.getBytes("UTF-8"));
-//            }
-//
-//            @Override
-//            public HttpHeaders getHeaders() {
-//                HttpHeaders header = new HttpHeaders();
-//                header.add("Content-Type","application/json;charsset=UTF-8");
-//                return header;
-//            }
-//        };
-//    }
-//
-//
-//}
+package en.tedu.sp11.fallback;
+
+import cn.tedu.web.util.JsonResult;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Haitao
+ * @date 2020/11/27 11:39
+ */
+@Component
+public class ItemFB implements FallbackProvider {
+    /**
+     * 返回 service-id
+     * 针对哪个服务进行降级
+     *
+     * "*"或 null是针对所有服务进行降级
+     * @return
+     */
+    @Override
+    public String getRoute() {
+        return "item-service";
+    }
+
+    /**
+     * 设置向客户端返回的降级响应
+     * @param route
+     * @param cause
+     * @return
+     */
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                //JsonResult - {code:200, msg:"调用后台商品服务失败", data:null}
+                String json = JsonResult.ok().msg("调用后台商品服务失败").toString();
+                return new ByteArrayInputStream(json.getBytes("UTF-8"));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders header = new HttpHeaders();
+                header.add("Content-Type","application/json;charsset=UTF-8");
+                return header;
+            }
+        };
+    }
+
+
+}
