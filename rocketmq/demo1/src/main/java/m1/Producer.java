d0bf85c090bcdf4d7932b63bfd81bfcde8242f0b
linziming <871440130@qq.com>
2020-12-15 23:55:43 +0800

    rocketmq initial

---
+++ rocketmq/demo1/src/main/java/m1/Producer.java
@@ -0,0 +1,19 @@
+package m1;
+
+public class Producer {
+    public static void main(String[] args) {
+        // 创建生产者
+
+        // 指定注册中心
+
+        // 启动生产者
+
+        // 向消息服务发送消息
+
+         /*
+        Topic 相当于是一级分类，
+        Tag 相当于是二级分类
+         */
+
+    }
+}
